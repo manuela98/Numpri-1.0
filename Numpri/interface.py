bfd762534b8aa1bfe5da86a7f129dac974886951
manuela98 <manujarare@hotmail.com>
2019-05-30 15:27:23 -0500

    Numpri

--- Numpri/interface.py
+++ Numpri/interface.py
@@ -9,14 +9,13 @@ import tkinter as tk
 from PIL import Image,ImageTk
 from time import sleep
 from Numpri import primos
-#import primos
 from functools import partial
 
 
 class Aplicacion(object):
 
     ''' 
-        Establece GUI para Numpri.
+        Establece los elementos para la interface grafica para Numpri.
     '''
 
 
@@ -52,8 +51,7 @@ class Aplicacion(object):
 
     def actualizarEntrada(self,cadenaEntrada):
         print(cadenaEntrada.get()) 
-        return cadenaEntrada.get() #... 
-
+        return cadenaEntrada.get() 
     def insertarEntrada(self,ventana,inicial):
 
        entrada = tk.StringVar(ventana)       
@@ -83,7 +81,7 @@ class Aplicacion(object):
        self.canvasImagen.create_image(anchoImagen/2.,largoImagen/2.,\
        image=imagenTk) 
        self.canvasImagen.image = imagenTk
-       return self.canvasImagen #....
+       return self.canvasImagen 
    
 
    
@@ -152,47 +150,8 @@ class  Conceptos(Aplicacion):
        
            
         
- #class Juego(Aplicacion):       
+     
 
-
-
-'''
-app = Aplicacion()
-ventana = app.crearVentanaPrincipal()
-app.insertarImagen(ventana,nombreImagen="../Img/ventanaInicial.ppm")
-app.ponerBoton(ventana,'Siguiente',comando=ventana.iconify)
-nuevaVentana = app.crearNuevaVentana("#F1E8B8")
-app.insertarImagen(nuevaVentana,"../Img/numerosPrimos.ppm")
-app.ponerBoton(nuevaVentana,'Calculadora',comando=nuevaVentana.iconify)
-app.ponerBoton(nuevaVentana,'Juego',comando=nuevaVentana.iconify)
-app.ponerBoton(nuevaVentana,'Conceptos',comando=nuevaVentana.iconify)
-
-
-calculadora = Calculadora()
-ventanaCalculadora = calculadora.crearNuevaVentana("#E9B872")
-botonesCalculadora = ['Es primo','Primos entre 2 numeros','CalcularNprimos']
-comandosCalculadora = [calculadora.activarEsPrimo,calculadora.activarPrimosIntervalo,\
-calculadora.activarCalcularNprimos]
-calculadora.insertarImagen(ventanaCalculadora,nombreImagen="../Img/calculadora.ppm")
-calculadora.crearMenu(ventanaCalculadora,botonesCalculadora,comandosCalculadora)
-
-
-
-'''
-conceptos = Conceptos()
-ventanaConceptos = conceptos.crearNuevaVentana("#E9B872")
-botonesConceptos = ['Curiosidades','Conjeturas']
-lista = ['Titulos1','Titulos2']
-comandoOpcion = partial(conceptos.crearMenuTitulos,lista)
-comandosConceptos = [comandoOpcion,comandoOpcion]
-conceptos.crearMenu(ventanaConceptos,botonesConceptos,comandosConceptos)
-'''
-
-
-
-
-ventana.mainloop()
-'''
 
 
 
