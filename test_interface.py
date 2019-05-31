4d7418f71ce8fe712e28d341d1019dfe1476d6fd
manuela98 <manujarare@hotmail.com>
2019-05-30 22:33:12 -0500

    final

--- test_interface.py
+++ test_interface.py
@@ -28,12 +28,6 @@ class NumpriTests(unittest.TestCase):
         self.ventana = Aplicacion.crearVentanaPrincipal(self.aplicacion)
         return isinstance(self.ventana,tk.Tk)
 
-    '''def test_insertarImagen(self):
-        self.aplicacion = Aplicacion()
-        self.ventana = Aplicacion.crearVentanaPrincipal(self.aplicacion)
-        self.canvas = Aplicacion.insertarImagen(self,self.ventana,nombreImagen="Img/ventanaInicial.ppm")
-        return isinstance(self.canvas,tk.Canvas)'''
-
             
     def test_crearNuevaVentana(self):
         self.aplicacion = Aplicacion()
@@ -57,12 +51,12 @@ class NumpriTests(unittest.TestCase):
         self.ventanaNueva = Aplicacion.crearNuevaVentana(self.aplicacion,"#3E8989")
         self.canvas = Aplicacion.mostrarRecuadroSalida(self.aplicacion,self.ventanaNueva,texto='Prueba Canvas')
         return isinstance(self.canvas,tk.Canvas)
-
      
 
     def test_actualizarEntrada(self):
         self.aplicacion = Aplicacion()
-        cadenaEntrada = tk.StringVar()
+        self.ventanaNueva = Aplicacion.crearNuevaVentana(self.aplicacion,"#3E8989")
+        cadenaEntrada = tk.StringVar(self.ventanaNueva)
         cadenaEntrada.set('prueba')
         cadenaSalida = Aplicacion.actualizarEntrada(self.aplicacion,cadenaEntrada)
         return self.assertEqual(cadenaSalida, cadenaEntrada.get())
@@ -72,6 +66,13 @@ class NumpriTests(unittest.TestCase):
         self.ventana = Aplicacion.crearVentanaPrincipal(self.aplicacion)
         recuadroEntrada = Aplicacion.insertarEntrada(self.aplicacion,self.ventana,'cadena de prueba')[1]
         return isinstance(recuadroEntrada,tk.Entry)
+
+    def test_valorActual(self):
+        self.aplicacion = Aplicacion()
+        cadenaEntrada = tk.StringVar()
+        cadenaEntrada.set('prueba')
+        cadenaSalida = Aplicacion.valorActualEntrada(self.aplicacion,cadenaEntrada)
+        return self.assertEqual(cadenaSalida.get(), cadenaEntrada.get())
 
     def test_crearMenu(self):
         self.aplicacion = Aplicacion()
@@ -91,23 +92,41 @@ class NumpriTests(unittest.TestCase):
         return True 
 
     def test_activarEsPrimo(self):
-        self.calculadora = Calculadora()  
+        self.calculadora = Calculadora() 
         self.ventana = Calculadora.crearNuevaVentana(self.calculadora,"#E9B872")
+        variable = tk.StringVar(self.ventana)
+        variable.set('7')
+        self.calculadora.valorEntrada = variable
         salida = Calculadora.activarEsPrimo(self.calculadora,self.ventana)
-        return self.assertEqual(salida,7)
-   
+        return self.assertEqual(salida,'7')
+
+    
     def test_activarPrimosIntervalo(self):
         self.calculadora = Calculadora()  
         self.ventana = Calculadora.crearNuevaVentana(self.calculadora,"#E9B872")
+        variable = tk.StringVar(self.ventana)
+        variable.set('7,11')
+        self.calculadora.valorEntrada = variable
         salidaIntervalo = Calculadora.activarPrimosIntervalo(self.calculadora,self.ventana)
-        return self.assertEqual(salidaIntervalo,['7','11'])
+        return self.assertEqual(salidaIntervalo,'7,11')
 
     def test_activarCalcularNprimos(self):
+        
         self.calculadora = Calculadora()  
         self.ventana = Calculadora.crearNuevaVentana(self.calculadora,"#E9B872")
+        variable = tk.StringVar(self.ventana)
+        variable.set('6')
+        self.calculadora.valorEntrada = variable
         salidaLista = Calculadora.activarCalcularNprimos(self.calculadora,self.ventana)
         listaPrimos = [2,3,5,7,11,13]
-        return self.assertEqual(salidaLista,listaPrimos)
+        return self.assertEqual(salidaLista,str(listaPrimos))
+    
+
+    def test_mostrarError(self):
+        self.calculadora= Calculadora()
+        ventana = Calculadora.mostrarError(self.calculadora,'Prueba')
+        return isinstance(ventana,tk.Tk)
+        
 
     def test_instanciadoConceptos(self):
         try:
